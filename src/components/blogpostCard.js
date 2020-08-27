5f79b502bdaae75445e72fe9dd61005f130be2fe
Angelo Martinez <amart670@fiu.edu>
2020-08-26 23:16:18 -0400

    added some more styles to tags

--- src/components/blogpostCard.js
+++ src/components/blogpostCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import { kebabCase } from 'lodash';
 import styles from '../styles/components/blogPostCard.module.scss';
 
 const BlogpostCard = ({ img, title, description, link, tags }) => {
+  const tagsItems = tags.map((tag) => (
+    <li key={tag + `tag`}>
+      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+    </li>
+  ));
   return (
     <article className={styles.card}>
       <Img fixed={img} className={styles.img} />
@@ -13,7 +19,7 @@ const BlogpostCard = ({ img, title, description, link, tags }) => {
           {description}
           <Link to={link}>Continue reading</Link>
         </p>
-        <p>{tags}</p>
+        <ul className={styles.tags}>{tagsItems}</ul>
       </div>
     </article>
   );
